e95ab94e7e94b2d8ca6a75dff15ac30b4cd37039
BestTTTA <bst12546@gmail.com>
2024-11-14 20:35:27 +0700

    update rows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,7 +106,7 @@ export default function Page() {
     }
   }, [isClient, accessToken]);
 
-  const totalRows = 20;
+  const totalRows = 21;
 
   const placeholderOrder: Order = {
     order_sn: "",
